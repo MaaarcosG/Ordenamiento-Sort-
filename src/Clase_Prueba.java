d9079cafc26a43c1809c121f1a3b6ab7b94d8e65
Alejandro-PC\Alejandro <mon17014@uvg.edu.gt>
2018-02-08 16:08:07 -0600

    Le agregamos una opción donde el usuario puede elegir si ordena un vector nuevo o uno ya ordenado

--- src/Clase_Prueba.java
+++ src/Clase_Prueba.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Scanner;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -11,26 +12,32 @@ public class Clase_Prueba {
 	
 	public static void main(String[] args) {
 		
-		//Creación del archivo
-		try {
+		System.out.println ("Desea calcular el tiempo de ordenación de un arreglo 1.ordenado ó 2.nuevo");
+        String entradaTeclado = "";
+        Scanner entradaEscaner = new Scanner (System.in); //Creación de un objeto Scanner
+        entradaTeclado = entradaEscaner.nextLine (); 
+        
+        if  (entradaTeclado.equals("2")) {
+        	//Creación del archivo
+        	try {
 			
-			//Se crea un objeto File, el cual crea o abre acceso a un archivo que se especifica en su constructor
-			File archivo=new File("texto.txt");
-			//Este objeto nos ayudara a escribir en el archivo
-			FileWriter escribir=new FileWriter(archivo);
-			//Escribimos en el archivo con el metodo write 
-			for (int i=0; i<3000;i++) {
-				int numero = (int) (Math.random() * 3000) + 1;
-				escribir.write(Integer.toString(numero)+"\n");
-			}
-			//Cerramos la conexion
-			escribir.close();
-		}
-		//Si existe un problema al escribir cae aqui
-		catch(Exception e){
-			System.out.println("Error al escribir");
-		}
-		
+        		//Se crea un objeto File, el cual crea o abre acceso a un archivo que se especifica en su constructor
+        		File archivo=new File("texto.txt");
+        		//Este objeto nos ayudara a escribir en el archivo
+        		FileWriter escribir=new FileWriter(archivo);
+        		//Escribimos en el archivo con el metodo write 
+        		for (int i=0; i<3000;i++) {
+        			int numero = (int) (Math.random() * 3000) + 1;
+        			escribir.write(Integer.toString(numero)+"\n");
+        		}
+        		//Cerramos la conexion
+        		escribir.close();
+        	}
+        	//Si existe un problema al escribir cae aqui
+        	catch(Exception e){
+        		System.out.println("Error al escribir");
+        	}
+        }
 		int [] arreglo = new int[3000];
 		
 		//lectura del archivo
@@ -59,6 +66,7 @@ public class Clase_Prueba {
 			System.out.println("Error al leer");
 		}
 		
+		entradaEscaner.close();
 	}
 	
 }
