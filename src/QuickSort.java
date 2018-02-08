cf592554604efe6121c7ed9007b33b4d257c4a99
MaaarcosG <33351664+MaaarcosG@users.noreply.github.com>
2018-02-08 09:23:32 -0600

    Cambiar los int dentro de las clases de quick

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,7 +1,21 @@
 /**
  * @author Marcos Gutierrez				17909
  * @author Raul Monzon					17014
+ * Clase para generar los datos
  */
 public class QuickSort {
-
+	public void quickSort(int array[], int low, int high) {
+		int pivot = array[high];
+		/*Indice para el numero mas pequeño */
+		int i = (low-1);
+		for(int j=low; j<high; j++) {
+			/*Si el elemento es pequeño, es igua al pivot*/
+			if(array[j] <= pivot) {
+				i++;
+				/*array[i] y array[j]*/
+				int temp = array[i];
+				
+			}
+		}
+	}
 }
