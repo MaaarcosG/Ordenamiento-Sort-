04c05cdc7668887519a3940d3f8ba2119a339cdd
MaaarcosG <33351664+MaaarcosG@users.noreply.github.com>
2018-02-09 09:33:37 -0600

    Creada la clase RandomNumbers

--- src/Clase_Prueba.java
+++ src/Clase_Prueba.java
@@ -11,7 +11,7 @@ import java.io.BufferedReader;
 public class Clase_Prueba {
 	
 	public static void main(String[] args) {
-		
+		RandomNumbers datos = new RandomNumbers();
 		System.out.println ("Desea calcular el tiempo de ordenación de un arreglo 1.ordenado ó 2.nuevo");
         String entradaTeclado = "";
         Scanner entradaEscaner = new Scanner (System.in); //Creación de un objeto Scanner
@@ -19,53 +19,9 @@ public class Clase_Prueba {
         
         if  (entradaTeclado.equals("2")) {
         	//Creación del archivo
-        	try {
-			
-        		//Se crea un objeto File, el cual crea o abre acceso a un archivo que se especifica en su constructor
-        		File archivo=new File("texto.txt");
-        		//Este objeto nos ayudara a escribir en el archivo
-        		FileWriter escribir=new FileWriter(archivo);
-        		//Escribimos en el archivo con el metodo write 
-        		for (int i=0; i<3000;i++) {
-        			int numero = (int) (Math.random() * 3000) + 1;
-        			escribir.write(Integer.toString(numero)+"\n");
-        		}
-        		//Cerramos la conexion
-        		escribir.close();
-        	}
-        	//Si existe un problema al escribir cae aqui
-        	catch(Exception e){
-        		System.out.println("Error al escribir");
-        	}
+        	datos.abrirArchivo();
         }
-		int [] arreglo = new int[3000];
-		
-		//lectura del archivo
-		//Creación del String que contendra todo el texto
-		try{
-			//FileReader obtiene lo que tenga el archivo
-			FileReader lector=new FileReader("texto.txt");
-			//BufferedReader guarda el contenido del lector
-			BufferedReader contenido=new BufferedReader(lector);
-			
-			int i=0;
-			String texto="";
-			
-			//Guardamos los numeros que tenía cada linea
-			while((texto=contenido.readLine())!=null)
-			{
-				arreglo[i]= Integer.parseInt(texto);
-				System.out.println(Integer.toString(arreglo[i]));
-				i++;
-			}
-			lector.close();
-		}
-
-		//Si se causa un error al leer cae aqui
-		catch(Exception e){
-			System.out.println("Error al leer");
-		}
-		
+        datos.conversionString();
 		entradaEscaner.close();
 	}
 	
